12f8fbbc043bc6e4411b1c0320552bd41bc65e0c
Abhishek Bhatnagar <abhishekbh@gmail.com>
2016-03-17 16:28:00 -0700

    minimalist rewrite

--- src/kvTrack.js
+++ src/kvTrack.js
@@ -1,76 +1,72 @@
+'use strict';
+
 /**
- * kvTrack - v0.0.1
- * Copyright (c) 2016 Kiva Microfunds
- *
- * Licensed under the MIT license.
- * http://github.com/kiva/fbkiva/license.txt
+ * @typedef {Object} Promise
  */
-define(['jquery'], function ($) {
-	'use strict';
 
-	/**
-	 * @typedef {Object} Promise
-	 */
+/**
+ *
+ * @param {string} uaID
+ * @param {Function} logHandler
+ * @constructor
+ */
+function kvTrack(uaID) {
+	var self = this
+	, trackDeferred = $.Deferred();
 
-	/**
-	 *
-	 * @param {string} uaID
-	 * @param {Function} logHandler
-	 * @constructor
-	 */
-	function kvTrack (uaID) {
+	// Local copies
+	this._gaID = '';
+	this.ga = null;
+	this.isReady = trackDeferred;
+
+	// Initialize Google Analytics
+	self.setUAId(uaID);
+	self.initGA();
+
+	// Contextualize all kvTrack methods
+	$.each(this, function (methodName, fn) {
+		if (typeof fn == 'function') {
+			self[methodName] = $.proxy(self, methodName);
+		}
+	});
+}
+
+
+kvTrack.prototype = {
+	init: function () {
+	}
+
+
+	, setUAId: function (uaID) {
+		this._gaID = uaID;
+	}
+
+
+	, initGA: function () {
 		var self = this;
-		this._gaID = '';
-		this.ga = null;
 
-		// Initialize Google Analytics
-		self.setUAId(uaID);
-		self.initGA();
-
-		// Contextualize all kvTrack methods
-		$.each(this, function (methodName, fn) {
-			if (typeof fn == 'function') {
-				self[methodName] = $.proxy(self, methodName);
-			}
+		// Get analytics.js from Google
+		$.ajax({
+			url: '//www.google-analytics.com/analytics.js'
+			, dataType: 'script'
+			, cache: true
+			, crossDomain: true // forces jQuery to create a script-tag as apposed to loading via ajax
+		}).fail(function(){
+			this.isReady.reject();
+		}).done(function(){
+			self.ga = window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+			self.ga('create', self._gaID, 'auto');
+			this.isReady.resolve();
 		});
 	}
 
 
-	kvTrack.prototype = {
-		init: function () {
-		}
-
-
-		, setUAId: function (uaID) {
-			this._gaID = uaID;
-		}
-
-
-		, initGA: function () {
-			var self = this;
-			// Get analytics.js from Google
-			$.ajax({
-				url: '//www.google-analytics.com/analytics.js'
-				, dataType: 'script'
-				, cache: true
-				, crossDomain: true // forces jQuery to create a script-tag as apposed to loading via ajax
-			}).fail(function(){
-			}).done(function(){
-				self.ga = window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-				self.ga('create', self._gaID, 'auto');
-			});
-		}
-
-
-		, trackEvent: function (category, action, label, value) {
-			this.ga('send', 'event', {
-				'eventCategory': category,
-				'eventAction': action,
-				'eventLabel': label,
-				'eventValue': value
-			});
-		}
-	};
-
-	return kvTrack;
-});
+	, trackEvent: function (category, action, label, value) {
+		this.ga('send', 'event', {
+			'eventCategory': String(category),
+			'eventAction': String(action),
+			'eventLabel': String(label),
+			'eventValue': parseInt(value)
+		});
+	}
+};
