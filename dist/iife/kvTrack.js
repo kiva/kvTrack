c96f4a4ec36b275ace8d2f85f54d219a33ef6b54
Abhishek Bhatnagar <abhishekbh@gmail.com>
2016-04-11 15:23:06 -0700

    build

--- dist/iife/kvTrack.js
+++ dist/iife/kvTrack.js
@@ -5,3 +5,79 @@
  * Licensed under the MIT license.
  * http://github.com/kiva/fbkiva/license.txt
  */
+(function ($, global) {
+	'use strict';
+	
+	/**
+	 * @typedef {Object} Promise
+	 */
+	
+	/**
+	 *
+	 * @param {string} uaID
+	 * @param {Function} logHandler
+	 * @constructor
+	 */
+	function kvTrack(uaID) {
+		var self = this
+		, trackDeferred = $.Deferred();
+	
+		// Local copies
+		this._gaID = '';
+		this.ga = null;
+		this.isReady = trackDeferred;
+	
+		// Initialize Google Analytics
+		self.setUAId(uaID);
+		self.initGA();
+	
+		// Contextualize all kvTrack methods
+		$.each(this, function (methodName, fn) {
+			if (typeof fn == 'function') {
+				self[methodName] = $.proxy(self, methodName);
+			}
+		});
+	}
+	
+	
+	kvTrack.prototype = {
+		init: function () {
+		}
+	
+	
+		, setUAId: function (uaID) {
+			this._gaID = uaID;
+		}
+	
+	
+		, initGA: function () {
+			var self = this;
+	
+			// Get analytics.js from Google
+			$.ajax({
+				url: '//www.google-analytics.com/analytics.js'
+				, dataType: 'script'
+				, cache: true
+				, crossDomain: true // forces jQuery to create a script-tag as apposed to loading via ajax
+			}).fail(function(){
+				this.isReady.reject();
+			}).done(function(){
+				self.ga = window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date; // jshint ignore:line
+				self.ga('create', self._gaID, 'auto'); // jshint ignore:line
+				self.isReady.resolve();
+			});
+		}
+	
+	
+		, trackEvent: function (category, action, label, value) {
+			this.ga('send', 'event', {
+				'eventCategory': String(category),
+				'eventAction': String(action),
+				'eventLabel': String(label),
+				'eventValue': parseInt(value)
+			});
+		}
+	};
+
+	global.kvTrack = kvTrack;
+}(jQuery, this));
