dc861838d8f0155cb8c7695ab6f8a903548804e8
Abhishek Bhatnagar <abhishekbh@gmail.com>
2016-04-13 23:31:51 -0700

    page tracker does not accept events fields

--- src/kvTrack.js
+++ src/kvTrack.js
@@ -90,21 +90,17 @@ kvTrack.prototype = {
 	/**
 	 * Google Analytics's Page View wrapper
 	 *
-	 * @param {string} path
-	 * @param {string} category
-	 * @param {string} action
-	 * @param {string} label
-	 * @param {int} value
+	 * @param {string} page
+	 * @param {string} title
+	 * @param {int} loc
 	 */
-	, trackPageView: function (path, category, action, label, value) {
-		label = (label !== undefined) ? String(label) : null;
-		value = (value !== undefined) ? parseInt(value) : null;
+	, trackPageView: function (page, title, loc) {
+		title = (label !== undefined) ? String(title) : null;
+		loc = (value !== undefined) ? String(loc) : null;
 
-		this.ga('send', 'pageview', String(path), {
-			'eventCategory': String(category),
-			'eventAction': String(action),
-			'eventLabel': label,
-			'eventValue': value
+		this.ga('send', 'pageview', String(page), {
+			'title': title,
+			'location': loc
 		});
 	}
 };
